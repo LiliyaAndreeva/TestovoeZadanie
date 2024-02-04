2261de8edc5ae827dfe45070f555d27d72dcf4ef
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-02-04 15:11:54 +0500

    Initial Commit

---
+++ TestovoeZadanie/TestovoeZadanieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestovoeZadanieApp.swift
+//  TestovoeZadanie
+//
+//  Created by Лилия Андреева on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestovoeZadanieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
